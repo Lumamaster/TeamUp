410ee9382c913c1bf4a343cc508a3af038b4a2db
Zach Bridwell <bridwellzac@ymail.com>
2019-09-12 01:39:16 -0400

    added react

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TeamUp from './TeamUp.png';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div id="top-border">
+        <img src={TeamUp} alt="" id="logo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
