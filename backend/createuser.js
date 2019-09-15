1c094f21dd6bd4b51a452d4068299197e5a096d5
burns140 <burns140@purdue.edu>
2019-09-15 19:37:36 -0400

    moved iteration functions for better organization

--- backend/createuser.js
+++ backend/createuser.js
@@ -1,17 +1,3 @@
-/* Iterates through the list, changing the BSON
-   objects to text and printing to console */
-function iterateFunc(doc) {
-    console.log(JSON.stringify(doc, null, 4));
-}
- 
-/* Catches and prints any errors that occur
-   when iterating through the list of BSON objects */
-function errorFunc(error) {
-    if (error != null) {
-        console.log(error);
-    }
-}
-
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
