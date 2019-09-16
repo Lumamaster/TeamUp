25609c4bdff36ad5e095c1b083fdcde8b5eec938
Alan Libby <alan.h.libby@gmail.com>
2019-09-15 22:18:43 -0400

    Add React-controlled form to Signup page

--- frontend/src/components/pages/SignupPage.js
+++ frontend/src/components/pages/SignupPage.js
@@ -1,13 +1,93 @@
 import React from 'react';
-//import TeamUp from '../../TeamUp.png';
 import '../../App.css';
 
-function SignupPage() {
-  return (
-    <div className="container">
-      <h1>Sign Up</h1>
-    </div>
-  );
+class SignupPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            password2: "",
+            errors: []
+        }
+    }
+    showErrors = () => {
+        return this.state.errors.map(err => <p className="color-error" key={err}>{err}</p>)
+    }
+    handleInputChange = e => {
+        const {name, value} = e.target;
+        this.setState({
+            [name]:value
+        });
+    }
+    handleLogin = e => {
+        e.preventDefault();
+        let errors = [];
+        if(!this.state.email || this.state.email.length === 0) {
+            errors.push('The email field cannot be blank.');
+        } else if(!this.state.email.match('^([A-Z]|[a-z]|[0-9]){1,480}@purdue\.edu$')) {
+            errors.push('Please enter a valid Purdue email address.')
+        }
+        if(!this.state.password || this.state.password.length === 0) {
+            errors.push('The password field cannot be blank.');
+        } else if(this.state.password.length > 140) {
+            /* TODO: Match a regex for the password. */
+            errors.push('Please enter a valid password.')
+        }
+        if(this.state.password !== this.state.password2) {
+            errors.push('The two passwords do not match.')
+        }
+
+        this.setState({
+            errors:errors
+        });
+
+        if(errors.length === 0) {
+            /* TODO submit this to the backend */
+            let payload = {
+                email: this.state.email,
+                password: this.state.password
+            }
+            console.log("POST to the signup endpoint:",payload);
+        }
+    }
+    render() {
+        return (
+            <div className="container">
+                <h1>Sign Up</h1>
+                <form>
+                    <input 
+                        name="email"
+                        type="text"
+                        onChange={this.handleInputChange}
+                        placeholder="Email"
+                        autoComplete="username"
+                    />
+                    <br/>
+                    <input
+                        name="password"
+                        type="password"
+                        onChange={this.handleInputChange}
+                        placeholder="Password"
+                        autoComplete="current-password"
+                    />
+                    <br/>
+                    <input
+                        name="password2"
+                        type="password"
+                        onChange={this.handleInputChange}
+                        placeholder="Confirm Password"
+                        autoComplete="current-password"
+                    />
+                    <br/>
+                    <button onClick={this.handleLogin}>Sign Up</button>
+                </form>
+                <div id="errors">
+                    {this.showErrors()}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default SignupPage;
