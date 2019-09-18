9e5d4f9ecd014b135d970ed34ad6375ee558184f
dhairyaa <amind@purdue.edu>
2019-09-18 00:18:53 -0400

    Removing history folder

--- .history/backend/listteams_20190917233501.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const cookie = require('../cookies');
-const dbconfig = require('../db_config.json');
-
