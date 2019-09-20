5f4d0dbea54af3a269e18d8c988e037cb1ccb4da
dhairyaa <amind@purdue.edu>
2019-09-19 22:49:38 -0400

    Updating list teams

--- backend/routes/listteams.js
+++ backend/routes/listteams.js
@@ -6,7 +6,7 @@ const cookie = require('../cookies');
 const dbconfig = require('../db_config.json');
 
 router.use(express.json());
-router.post('/listteams', async(req,res) => {
+router.post('/', async(req,res) => {
     if(cookie.readCookie("") == null) {
         // Redirect to login page
         res.status(400).json({message:"not logged in"})
@@ -103,3 +103,5 @@ router.post('/teamsearch', async(req,res) => {
     
 
 });
+
+module.exports = router;
