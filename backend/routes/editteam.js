146d5b65651f28400121ac99160a8d6ca52cfae8
Alan Libby <alan.h.libby@gmail.com>
2019-10-08 20:25:57 -0400

    Change editteam.js to use jwt

--- backend/routes/editteam.js
+++ backend/routes/editteam.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 const assert = require('assert');
-const cookie = require('../cookies');
+const verify = require('../verifyjwt');
 const dbconfig = require('../db_config.json');
 
+router.use(verify);
 router.use(express.json());
-router.post('/', async(req,res) => {
-    if(cookie.readCookie("") == null) {
-        // Redirect to login page
-        res.status(400).json({message:"not logged in"})
-        return;
-    }
+router.post('/:id', async(req,res) => {
 
-    const {teamid, teamName, teamMembers, owner, info, requestedSkills, numMembers, open, course, maxMembers} =  req.body;
+    const user = req.token;
+    const teamId = req.params.id;
+
+    const {teamName, teamMembers, info, requestedSkills, numMembers, open, course, maxMembers} =  req.body;
 
     // Update team data
     try{
 
-        MongoClient.connect(dbconfig.url, { useNewUrlParser: true, useUnifiedTopology: true}, function(err,client){
+        MongoClient.connect(dbconfig.url, { useNewUrlParser: true, useUnifiedTopology: true}, async function(err,client){
             assert.equal(null, err);
             const db = client.db("Teams");
 
@@ -26,7 +26,6 @@ router.post('/', async(req,res) => {
         var team = {
             teamName: teamName,
             teamMembers: teamMembers,
-            owner: owner,
             info: info,
             requestedSkills: requestedSkills,
             numMembers: numMembers,
@@ -37,8 +36,22 @@ router.post('/', async(req,res) => {
         };
 
         // Update team data on the database
-        db.collection('team').update({_id: teamid}, {team});
+        const foundTeam = await db.collection('team').findOne({_id:ObjectId(teamId)});
+        if(!foundTeam) {
+            res.status(404).json({err:"Couldn't find that team"})
+        }
+        console.log(foundTeam.owner);
+        console.log(user);
+        if(foundTeam.owner.id !== user.id) {
+            res.status(401).json({err:"You are not the owner of that team."});
+            client.close();
+            return;
+        }
+        await db.collection('team').updateOne({_id: ObjectId(teamId)}, {
+            $set: team
+        });
 
+        res.status(200).send();
         client.close();
         });
 
