0ec87ccf01d8291ebc1c392b51f918416888c26e
Stephen Burns <burns140@purdue.edu>
2019-10-17 12:24:18 -0400

    added getting and setting free times

--- backend/routes/viewcommontimes.js
+++ backend/routes/viewcommontimes.js
@@ -10,6 +10,46 @@ const jwt = require('jsonwebtoken');
 router.use(verify);
 router.use(express.json());
 router.use(express.urlencoded({extended:false}));
+
+router.get('/user/get/:id', async (req, res) => {
+    const userId = req.params.id;
+    if(userId.length !== 24){
+        console.log("invalid team ID");
+        res.status(400).json({err:"Invalid team ID"}).send();
+        return;
+    }
+
+    try {
+        MongoClient.connect(dbconfig.url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+            assert.equal(null, err);
+            const userdb = client.db("Users");
+
+            /* Find the user with the passed in id */
+            var user = userdb.collection('user').findOne({
+                "_id":ObjectID(userId)
+            });
+
+            /* Return this user's time array */
+            user.then(function (result) {
+                res.status(200).json({times:result.times});
+                console.log(result.times);
+                client.close();
+                return;
+            }).catch(function (err) {
+                res.status(400).json({err:err});
+                console.log(err);
+                client.close();
+                return;
+            });
+
+            
+        });
+    } catch(err) {
+        console.error(err);
+    } finally{}
+
+});
+
 router.get('/:id', async (req,res) => {
 
     /* Check that the team id is valid */
@@ -23,7 +63,7 @@ router.get('/:id', async (req,res) => {
     try {
         MongoClient.connect(dbconfig.url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
             assert.equal(null, err);
-            const userdb
+            const userdb = client.db("Users");
 
             /* Find all users that have an element with this teamID in their curTeams array */
             var users = userdb.collection('user').find({
@@ -50,6 +90,46 @@ router.get('/:id', async (req,res) => {
                 client.close();
                 return;
             });
+            
+        });
+    } catch(err) {
+        console.error(err);
+    } finally{}
+
+});
+
+router.post('/user/get/:id', async (req, res) => {
+    const userId = req.params.id;
+    const freeTimes = req.body.times;
+
+    if(userId.length !== 24){
+        console.log("invalid team ID");
+        res.status(400).json({err:"Invalid team ID"}).send();
+        return;
+    }
+
+    try {
+        MongoClient.connect(dbconfig.url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+            assert.equal(null, err);
+            const userdb = client.db("Users");
+
+            db.collection('user').updateOne(
+                { _id:ObjectId(userId) },
+                {
+                    $set: { times: freeTimes }
+                }
+            ).then(function (result) {
+                res.status(200).json({success:'free times updated'});
+                console.log('free times updated');
+                client.close();
+                return;
+            }).catch(function (err) {
+                res.status(400).json({err:err});
+                console.log(err);
+                client.close();
+                return;
+            })
+            
             
         });
     } catch(err) {
