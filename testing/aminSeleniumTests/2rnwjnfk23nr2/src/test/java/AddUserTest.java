4a72a3f3341b80df3bdad94a0f7bab0f6de21280
dhairyaa <amind@purdue.edu>
2019-10-19 14:03:18 -0400

    Adding notification,add user & edit team tests

---
+++ testing/aminSeleniumTests/2rnwjnfk23nr2/src/test/java/AddUserTest.java
@@ -0,0 +1,165 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
+public class AddUserTest {
+
+    //Checking inviting user as well as notifying them about invite
+
+    @Test
+    public void testSuccessfulAddUser(){
+
+        try{
+            URL url = new URL("http://localhost:8000/");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection)con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            byte[] out = "{\"invitee\":\"delete@purdue.edu\",\"teamid\":\"\", \"teamname\":\"\" }".getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            http.connect();
+            try(OutputStream os = http.getOutputStream()) {
+                os.write(out);
+            }
+
+            BufferedReader in;
+            int statuscode = ((HttpURLConnection) con).getResponseCode();
+            if (statuscode >= 400) {
+                in = new BufferedReader(new InputStreamReader(((HttpURLConnection) con).getErrorStream()));
+            } else {
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+            String decodedString;
+            String lastString = "";
+            while ((decodedString = in.readLine()) != null) {
+                if (decodedString != null) {
+                    lastString = decodedString;
+                }
+                System.out.println(decodedString);
+            }
+            in.close();
+
+            Assert.assertEquals( "added invite successfully", lastString);
+
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    // Testing accepting an invite
+    @Test
+    public void testAcceptInvite(){
+        try{
+            URL url = new URL("http://localhost:8000/acceptinvite");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection)con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            byte[] out = "{\"teamid\":\"\",\"userid\":\"\" }".getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            http.connect();
+            try(OutputStream os = http.getOutputStream()) {
+                os.write(out);
+            }
+
+            BufferedReader in;
+            int statuscode = ((HttpURLConnection) con).getResponseCode();
+            if (statuscode >= 400) {
+                in = new BufferedReader(new InputStreamReader(((HttpURLConnection) con).getErrorStream()));
+            } else {
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+            String decodedString;
+            String lastString = "";
+            while ((decodedString = in.readLine()) != null) {
+                if (decodedString != null) {
+                    lastString = decodedString;
+                }
+                System.out.println(decodedString);
+            }
+            in.close();
+
+            Assert.assertEquals( "accepted invite successfully", lastString);
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // Testing declining an invite
+    @Test
+    public void testDeclineInvite(){
+        try{
+
+            URL url = new URL("http://localhost:8000/declineinvite");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection)con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            byte[] out = "{\"teamid\":\"\",\"userid\":\"\" }".getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            http.connect();
+            try(OutputStream os = http.getOutputStream()) {
+                os.write(out);
+            }
+
+            BufferedReader in;
+            int statuscode = ((HttpURLConnection) con).getResponseCode();
+            if (statuscode >= 400) {
+                in = new BufferedReader(new InputStreamReader(((HttpURLConnection) con).getErrorStream()));
+            } else {
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+            String decodedString;
+            String lastString = "";
+            while ((decodedString = in.readLine()) != null) {
+                if (decodedString != null) {
+                    lastString = decodedString;
+                }
+                System.out.println(decodedString);
+            }
+            in.close();
+
+            Assert.assertEquals( "declined invite successfully", lastString);
+
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // Testing the email of user to be added
+    @Test
+    public void testInvalidEmail(){
+        try{
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
