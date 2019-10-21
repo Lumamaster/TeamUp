e6d5b4f8dbea9290346d9fda94a4e5a24df7de3f
burns140 <burns140@purdue.edu>
2019-10-20 21:25:12 -0400

    added client closures

--- backend/routes/jointeam.js
+++ backend/routes/jointeam.js
@@ -40,10 +40,12 @@ router.get('/:id', async (req,res) => {
 
             if(team.length === 0) {
                 res.status(404).json({err:"Couldn't find that team"})
+                client.close();
                 return;
             }
             if(user.length === 0 || user.length > 1 || team.length > 1) {
                 res.status(500).send()
+                client.close();
                 return;
             }
             team = team[0]
@@ -53,14 +55,17 @@ router.get('/:id', async (req,res) => {
             //console.log(user)
             user.curTeams.forEach(team => {
                 if(team._id === ObjectID(teamID)) {
-                    res.status(400).send({err:"You are already in that team."})
+                    res.status(400).json({err:"You are already in that team."})
                     isInTeam = true;
                 }
             })
-            if(isInTeam) return;
-
+            if(isInTeam) {
+                client.close();
+                return;
+            }
             if(!team.alive) {
-                res.status(400).send({err:"That team is no longer active."})
+                res.status(400).json({err:"That team is no longer active."});
+                client.close();
                 return;
             }
 
@@ -119,6 +124,7 @@ router.get('/:id', async (req,res) => {
                 
                 await Promise.all([teamupdate,userupdate])
                 res.status(200).send("user joined notified");
+                client.close();
                 return;
             } else {
                 //Request to join the team
@@ -138,12 +144,14 @@ router.get('/:id', async (req,res) => {
                 })
                 await Promise.all([teamupdate,userupdate])
                 res.status(200).send("user requested notified");
+                client.close();
                 return;
             }
         });
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
+        client.close();
         return;
     }
 })
